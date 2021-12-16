413953afb83139f819919357e44d477f787b2905
DoHn <lucafarci@protonmail.com>
2021-12-16 21:53:34 +0100

    fixed character escaping in welcome modal

--- src/components/home-page/organisms/WelcomeModal.tsx
+++ src/components/home-page/organisms/WelcomeModal.tsx
@@ -13,21 +13,21 @@ export default function WelcomeModal() {
     <div className="absolute w-full h-full bg-alphagray flex justify-center items-center">
       <div className="flex flex-col justify-center items-center p-16 bg-gray-900 rounded w-96">
         <span className="text-white font-semibold text-center mb-2">
-          Looks like it's your first time.
+          Looks like it&apos;s your first time.
         </span>
         <span className="text-gray-500 text-center">
           Would you like to add your match history to our database?
         </span>
         <Link href="/add-games">
           <a className="text-white rounded bg-green-600 hover:bg-green-500 py-2 text-center text-sm mt-8 w-full font-semibold">
-            Yes, I'd like to add my games.
+            Yes, I&apos;d like to add my games.
           </a>
         </Link>
         <button
           onClick={handleClosureClick}
           className="text-white text-sm mt-2 bg-gray-800 hover:bg-gray-700 w-full py-2 rounded"
         >
-          No, I'll use other people's games.
+          No, I&apos;ll use other people's games.
         </button>
       </div>
     </div>
