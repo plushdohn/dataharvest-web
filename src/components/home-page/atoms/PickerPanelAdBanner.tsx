8c1bfed5cf3b14894bdebd36c0848479d6a4d0c5
DoHn <lucafarci@protonmail.com>
2021-12-21 16:11:30 +0100

    fixed picker panel ad

--- src/components/home-page/atoms/PickerPanelAdBanner.tsx
+++ src/components/home-page/atoms/PickerPanelAdBanner.tsx
@@ -1,9 +1,9 @@
 export default function PickerPanelAdBanner() {
   return (
     <div
-      className="_fa7cdd4c68507744 bg-red-500"
+      className="_fa7cdd4c68507744 self-start"
       data-zone="f075fe8fa2ff443da9c7923d39df73a8"
-      style={{ width: "234px", height: "60px", display: "inline-block" }}
+      style={{ width: "234px", height: "60px" }}
     />
   );
 }
