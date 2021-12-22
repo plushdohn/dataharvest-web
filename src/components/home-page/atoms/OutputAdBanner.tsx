983f459d9049983695145e2135b219244b9237ec
DoHn <lucafarci@protonmail.com>
2021-12-22 02:24:35 +0100

    modified ad script again

--- src/components/home-page/atoms/OutputAdBanner.tsx
+++ src/components/home-page/atoms/OutputAdBanner.tsx
@@ -20,8 +20,8 @@ export default function OutputAdBanner() {
       ) : (
         <div
           className="_fa7cdd4c68507744"
-          data-zone="f075fe8fa2ff443da9c7923d39df73a8"
-          style={{ width: "234px", height: "60px" }}
+          data-zone="196961007e564751aaf6adc5d603fda8"
+          style={{ width: "320px", height: "100px" }}
         />
       )}
     </>
